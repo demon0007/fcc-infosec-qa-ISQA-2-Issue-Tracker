0fb71a5e97543892300161e22228f48e8d1338d3
Glitch (fallacious-streetcar) <none>
2019-02-01 16:32:32 +0000

    🚇👕 Checkpoint ./views/index.html:2619925/54 ./routes/api.js:2619925/1505

--- routes/api.js
+++ routes/api.js
@@ -11,23 +11,28 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-
+var gdb
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
-
-  MongoClient.connect(CONNECTION_STRING, (err, db) => {
+MongoClient.connect(CONNECTION_STRING, (err, db) => {
     
     if (err) {
       console.log("Error In Connecting To the Database")
     } else {
       
       console.log("Successfully Connected to The Database")
-      app.route('/api/issues/:project')
+      gdb = db
+      }
+  })
+
+module.exports = function (app) {
+
+    app.route('/api/issues/:project')
   
       .get(function (req, res){
         var project = req.params.project;
-
+        console.log(project)
+        res.json({})
       })
     
       .post(function (req, res){
@@ -45,8 +50,5 @@ module.exports = function (app) {
         var project = req.params.project;
 
       });
-      }
-    
-  })
     
 };
