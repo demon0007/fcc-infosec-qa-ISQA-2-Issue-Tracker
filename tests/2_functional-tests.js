7e36fea90af29acda934a1bf3a4af9ecbbc99606
Glitch (fallacious-streetcar) <none>
2019-02-02 17:20:19 +0000

    🌓😎 Checkpoint ./tests/2_functional-tests.js:2619925/2017

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -29,7 +29,11 @@ suite('Functional Tests', function() {
         })
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res)
+          assert.equal(res.body.issue_title, 'Title')
+          assert.equal(res.body.issue_text, 'text')
+          assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
+          assert.equal(res.body.assigned_to, 'Chai and Mocha')
+          assert.equal(res.body.statue_text, 'In QA')
           //fill me in too!
           
           done();
@@ -37,11 +41,49 @@ suite('Functional Tests', function() {
       });
       
       test('Required fields filled in', function(done) {
-        
+        chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          // assigned_to: 'Chai and Mocha',
+          // status_text: 'In QA'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, 'Title')
+          assert.equal(res.body.issue_text, 'text')
+          assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
+          // assert.equal(res.body.assigned_to, 'Chai and Mocha')
+          // assert.equal(res.body.statue_text, 'In QA')
+          //fill me in too!
+          
+          done();
+        });
       });
       
       test('Missing required fields', function(done) {
-        
+        chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          // created_by: 'Functional Test - Every field filled in',
+          // assigned_to: 'Chai and Mocha',
+          // status_text: 'In QA'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, 'Title')
+          assert.equal(res.body.issue_text, 'text')
+          assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
+          // assert.equal(res.body.assigned_to, 'Chai and Mocha')
+          // assert.equal(res.body.statue_text, 'In QA')
+          //fill me in too!
+          
+          done();
+        });
       });
       
     });
